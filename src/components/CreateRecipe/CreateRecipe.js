777a52f4c156f4d7275ce299196a59f19cc6f7cb
bairdh <hrbaird10@gmail.com>
2020-05-27 11:43:52 -0500

    set up base functionality for create Recipe page

--- src/components/CreateRecipe/CreateRecipe.js
+++ src/components/CreateRecipe/CreateRecipe.js
@@ -1,19 +1,82 @@
 import React, { Component } from "react";
-import { Box, Typography, TextField } from "@material-ui/core";
+import { Box, Typography, TextField, Button } from "@material-ui/core";
+import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 
 class CreateRecipe extends Component{
+
+    state = {
+        title: '',
+        image: '',
+        ingredient: '',
+        direction: '',
+        ingredients: [],
+        directions: []
+    }
+
+    handleChange = (event, prop) => {
+        this.setState({
+            [prop]: event.target.value
+        })        
+    }
+
+    ingredientList = () => {
+        this.setState({
+           ingredients: [...this.state.ingredients, this.state.ingredient],
+        })
+    }
+    
+    directionList = () => {
+        this.setState({
+           directions: [...this.state.directions, this.state.direction],
+        })
+    }
+
     render(){
         return(
             <Box>
                 <Typography variant="h3">Create Recipe</Typography>
-                <TextField variant="standard"
-                    label="Recipe Title"/>
-                <TextField variant="standard"
-                    label="Recipe Image"/>
-                <TextField variant="standard"
-                    label="Recipe Ingredient"/>
-                <TextField variant="standard"
-                    label="Recipe directions"/>
+                <TextField onChange={event => this.handleChange(event, "title")} variant="standard"
+                    label="Title"/>
+                <TextField onChange={event => this.handleChange(event, "image")}variant="standard"
+                    label="Image"/>
+                <Box>
+                    <Box>
+                        <Typography>Ingredients:</Typography>
+                        <ul>
+                            {this.state.ingredients.map((item, i) => (
+                                <li key={i}>{item}</li>
+                            ))}
+                        </ul>
+
+                    </Box> {/* End Ingredient List */}
+                    <TextField onChange={event => this.handleChange(event, "ingredient")}
+                        ref="ingredient" 
+                        variant="standard"
+                        label="Ingredient"/>
+                    <Button onClick={this.ingredientList} 
+                        variant="outlined" 
+                        color="secondary">Add Ingredient</Button>
+                </Box> {/* End Ingredient Box */} 
+                <Box>
+                    <Box>
+                        <Typography>Directions:</Typography>
+                        <ol>
+                            {this.state.directions.map((step, i) => (
+                                <li key={i}>{step} < EditOutlinedIcon fontSize="small" onClick={}/><RemoveCircleOutlineIcon fontSize="small"/> </li>
+                            ))}
+                        </ol>
+                    </Box> {/* End Direction List */}
+                    <TextField onChange={event => this.handleChange(event, "direction")} 
+                        variant="outlined"
+                        multiline={true}
+                        label="Directions"/>
+                    <Button onClick={this.directionList}
+                        variant="outlined" 
+                        color="secondary">Add Direction</Button>
+                        <br/>
+                </Box>  {/* End Direction Box */}
+                <Button variant="outlined">Submit all</Button>
             </Box>
         ) // return
     } // render
